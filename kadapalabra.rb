6263cafc8f582ded93232e2baa44b904481fca9f
Manuel Ortega <manuel@lasindias.coop>
2017-08-29 13:41:28 +0200

    Agregando un funshon pa subí un imágin i us'é den e publikashon di e palabra

--- kadapalabra.rb
+++ kadapalabra.rb
@@ -1,5 +1,25 @@
 require "rubygems"
 require "mogli"
+require 'RMagick'
+
+def create_upload_image(client, name)
+
+  canvas = Magick::Image.new(1200, 628){self.background_color = '#002b7f'}
+  text = Magick::Draw.new
+  text.font_family = 'lavi'
+  text.pointsize = 140
+  text.gravity = Magick::CenterGravity
+
+  text.annotate(canvas, 0,0,2,2, name) {
+    self.fill = 'white'
+    self.font_weight = Magick::BoldWeight
+  }
+
+  canvas.write(name + ".png")
+
+  return client.post("me/photos", nil, {:source => File.open(name + ".png")})
+
+end
 
 access_token = 'ACCES_TOKEN'
 page_id = 'PAGE_ID'
@@ -20,11 +40,16 @@ first_line = File.open("dikshonario.txt").first
 unless first_line.nil?
   puts first_line
   post_data = {}
-  post_data[:message] = first_line
+  post_data[:message] = first_line + ' https://www.papiamentu.info/palabra/' + first_line + ' #papiamentu'
   post_data[:name]    = first_line
-  post_data[:link]    = 'https://www.papiamentu.info/palabra/' + first_line
   post_data[:caption] = first_line
   post_data[:description] = first_line
+
+  image = create_upload_image(page_client, first_line)
+
+  post_data[:object_attachment] = image["id"]
+
+  puts post_data
 
   published = true if page_client.post("feed", nil, post_data)
 
