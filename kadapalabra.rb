7dcd82588790567775643379a5056695f492e8b9
Manuel Ortega <manuel@lasindias.coop>
2017-09-17 12:42:37 +0200

    New version using OOP

--- kadapalabra.rb
+++ kadapalabra.rb
@@ -1,112 +1,108 @@
-require "rubygems"
-require "mogli"
+require 'rubygems'
+require 'mogli'
 require 'RMagick'
 require 'httparty'
-require "addressable/uri"
+require 'addressable/uri'
 require 'json'
 
-def create_upload_image(client, name)
+access_token = ''
+page_id = ''
+published = false
+dikshonario_file = File.join(File.dirname(__FILE__), 'dikshonario.txt')
 
-  canvas = Magick::Image.new(1200, 628){self.background_color = '#002b7f'}
-  text = Magick::Draw.new
-  text.font_family = 'lavi'
-  text.pointsize = 140
-  text.gravity = Magick::CenterGravity
+class Palabra
+  attr_reader :lexeme
+  @@papiamentu_api = 'http://sifma.dijkhofflearning.com'
+  @@abclink_api = 'http://abclink.info'
 
-  text.annotate(canvas, 0,0,2,2, name) {
-    self.fill = 'white'
-    self.font_weight = Magick::BoldWeight
-  }
-
-  def data_foi_banko(palabra)
-    url = "http://sifma.dijkhofflearning.com/word/#{palabra}"
-    response = HTTParty.get(Addressable::URI.parse(url).normalize.to_s)
-    #JSON.parse(response)
-    #puts response
+  def initialize(lexeme)
+    @lexeme = lexeme
+    set_data
+    @variants = variants
   end
 
-  def data_foi_bankortografiko(palabra)
-    url = "http://abclink.info/palabra/#{palabra}"
-    response = HTTParty.get(Addressable::URI.parse(url).normalize.to_s)
-    #JSON.parse(response)
+  def post
+    post_data = {}
+    post_data[:message] = "#{@lexeme} https://www.papiamentu.info/palabra/#{@lexeme} \n"
+    post_data[:name]    = @lexeme
+    post_data[:caption] = @lexeme
+    post_data[:description] = @lexeme
+    post_data[:object_attachment] = @facebook_image
+    post_data[:message] += "Informashon tokante e palabra: #{@buki_di_oro} \n"
+    post_data[:message] += 'Aprobá: ' << (@aproba ? 'Si' : 'No') << " \n"
+    post_data[:message] += 'Standarisá: ' << (@standarisa ? 'Si' : 'No') << " \n"
+    post_data[:message] += "Variantenan ortográfiko: #{@variants} \n" if @variants != ''
+    post_data[:message] += '#papiamentu'
+    post_data
   end
 
-  canvas.write(name + ".png")
+  def set_data
+    # https://www.papiamentu.info/palabra/televishon
+    url = "#{@@papiamentu_api}/word/#{@lexeme}"
+    data = HTTParty.get(Addressable::URI.parse(url).normalize.to_s)
+    @buki_di_oro = data[0]['buki_di_oro_text']
+    @aproba = data[0]['buki_di_oro'] == 1
+    @standarisa = data[0]['attested'] == 1
+  end
 
-  return client.post("me/photos", nil, {:source => File.open(name + ".png")})
+  def variants
+    variants = ''
+    url = "#{@@abclink_api}/palabra/#{@lexeme}"
+    response = HTTParty.get(Addressable::URI.parse(url).normalize.to_s)
+    data = JSON.parse(response.body)
+    arr = []
+    data.each_with_index { |v, i| arr.include?(v['lexeme']) ? data.delete_at(i) : arr << v['lexeme'] }
 
+    return variants if data.count <= 1
+    data.each do |variante|
+      variants += "#{variante['orthographic_type']}: #{variante['lexeme']} "
+    end
+
+    variants
+  end
+
+  def publish_image(client)
+    canvas = Magick::Image.new(1200, 628) { self.background_color = '#002b7f' }
+    text = Magick::Draw.new
+    text.font_family = 'lavi'
+    text.pointsize = 140
+    text.gravity = Magick::CenterGravity
+
+    text.annotate(canvas, 0, 0, 2, 2, @lexeme) do
+      self.fill = 'white'
+      self.font_weight = Magick::BoldWeight
+    end
+    image_file = File.join(File.dirname(__FILE__), "#{@lexeme}.png")
+    canvas.write(image_file)
+
+    image = client.post('me/photos', nil, source: File.open(image_file))
+    @facebook_image = image['id']
+  end
 end
 
-access_token = 'ACCESS_TOKEN'
-page_id = 'PAGE_ID'
-published = false
-
 client = Mogli::Client.new(access_token)
-user = Mogli::User.find("me",client)
+user = Mogli::User.find('me', client)
 
 page = user.accounts.select do |account|
   account.id.to_s == page_id
 end.first
 
-page = Mogli::Page.new(:access_token => page.access_token)
+page = Mogli::Page.new(access_token: page.access_token)
 page_client = page.client_for_page
 
-first_line = File.open("dikshonario.txt").first.strip
+first_line = File.open(dikshonario_file).first
+raise ArgumentError, 'The file your are reading is empty' if first_line.nil?
+first_line.strip!
 
-unless first_line.nil?
-  puts first_line
-  post_data = {}
-  post_data[:message] = first_line + ' https://www.papiamentu.info/palabra/' + first_line + " \n"
-  post_data[:name]    = first_line
-  post_data[:caption] = first_line
-  post_data[:description] = first_line
+palabra = Palabra.new(first_line)
+palabra.publish_image(page_client)
+puts palabra.lexeme
+puts palabra.post
 
-  # Imagen
-  image = create_upload_image(page_client, first_line)
-  post_data[:object_attachment] = image["id"]
+published = true if page_client.post('feed', nil, palabra.post)
 
-  # Informashon for di Banko di Palabra
-  data = data_foi_banko(first_line)
-
-  post_data[:message] += 'Informashon tokante e palabra: ' + data[0]["buki_di_oro_text"] + " \n"
-
-  if data[0]["buki_di_oro"] == 1
-    aproba_text = "Si"
-  else
-    aproba_text = "No"
-  end
-
-  if data[0]["attested"] == 1
-    standarisa_text = "Si"
-  else
-    standarisa_text = "No"
-  end
-
-  post_data[:message] += 'Aprobá: '  + aproba_text + " \n"
-  post_data[:message] += 'Standarisá: ' + standarisa_text + "\n"
-
-  variantenan = data_foi_bankortografiko(first_line)
-
-  unless variantenan.count <= 1
-    text = ''
-    variantenan.each do |variante|
-      text += "#{variante["orthographic_type"]}: #{variante["lexeme"]} "
-    end
-    post_data[:message] += 'Variantenan ortográfiko: '  + text + " \n"
-  end
-
-  post_data[:message] += '#papiamentu'
-
-  puts post_data
-
-  published = true if page_client.post("feed", nil, post_data)
-
-  if published
-    text=''
-    File.open("dikshonario.txt","r"){|f|f.gets;text=f.read}
-    File.open("dikshonario.txt","w+"){|f| f.write(text)}
-  end
-
-else
-  puts "The file your are reading is empty"
+if published
+  text = ''
+  File.open(dikshonario_file, 'r') { |f| f.gets; text = f.read }
+  File.open(dikshonario_file, 'w+') { |f| f.write(text) }
 end
